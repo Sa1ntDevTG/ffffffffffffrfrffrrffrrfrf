58034ca4f943fc91676496ba74ae5bfc10858a62
Sa1ntDevTG <101469457+Sa1ntDevTG@users.noreply.github.com>
2022-03-18 02:33:25 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+token = '5102222785:AAGZKgCbHEpGnXy_uPuEU1cYTvEe3cf5a7g' # Токен бота
+loginbot = '@eyeofgodtbot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'BOTIZM' #p2p public key
+QIWI_TOKEN = 'BOTIZM' # токен киви
+QIWI_NUMBER = '77051616206' # Номер киви без +
+
+admin_id = 5006820524 # Ваш айди
+
+price = 50 # Цена за доступ
+
+#Ссылка на канал - @slivmenss
+#Наш чат - @slivmens_chat
